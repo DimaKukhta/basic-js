707ea8e05279bb8038316c27650fd3fd3414499b
Dima Kukhta <dima.kukhta.2001@mail.ru>
2020-10-31 18:39:58 +0200

    feat:all test passed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(backyard) {
+  let result = 0;
+  backyard.forEach((value) => {
+      value.forEach((i) => {
+          if (i === "^^") {
+              result++;
+          }
+      });
+  });
+  return result;
+  };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addition = "";
+  if (options.addition !== undefined) {
+      addition = String(options.addition);
+  }
+
+  let addResult = addition;
+
+  str = String(str);
+
+  let sep = options.separator ? options.separator : "+";
+  let addSep = options.additionSeparator ? options.additionSeparator : "|";
+
+  if (addition) {
+      for (let i = 0; i < options.additionRepeatTimes - 1; i++) {
+          addResult += addSep + addition;
+      }
+  }
+
+  let result = str + addResult;
+  for (let i = 0; i < options.repeatTimes - 1; i++) {
+      if (addition) {
+          result += sep + str + addResult;
+      } else {
+          result += sep + str;
+      }
+  }
+  return result;
 };
   
